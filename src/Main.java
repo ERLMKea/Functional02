2a87408508c4a5c2f28fc47b87fd7f13a7075114
ERLMKea <erlm@kea.dk>
2022-02-11 15:05:08 +0100

    generate 12 random numre

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Locale;
 import java.util.function.Consumer;
@@ -10,33 +11,54 @@ import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void infiniteLoop() {
         Supplier<Double> ran = () -> Math.random();
         Stream<Double> dblrandom = Stream.generate(ran);
+        dblrandom.forEach(s -> System.out.println(s));
+    }
 
-        double dd = Math.random();
-        Long ii = Math.round(dd*100);
-        System.out.println(ii);
-
+    public static void generate12EqualNumbers() {
+        Supplier<Double> ran = () -> Math.random();
         Function<Double, Long> dblround = d -> Math.round(d*100);
-
-        Predicate<Long> longEqual = l -> l % 2 == 0;
-
-        Consumer<Long> print = i -> System.out.println("Rand=" + i);
-
-        System.out.println("test ran called");
         Stream<Long> lstLong =  Stream.generate(ran).map(dblround);
-        lstLong= lstLong.filter(longEqual).limit(12);
+        Predicate<Long> longEqual = l -> l % 2 == 0;
+        lstLong.filter(longEqual).limit(12).forEach(System.out::println);
+    }
 
+    public static void useAggregateFunctions() {
+        Supplier<Double> ran = () -> Math.random();
+        Function<Double, Long> dblround = d -> Math.round(d*100);
+        Stream<Long> lstLong =  Stream.generate(ran).map(dblround);
+        Predicate<Long> longEqual = l -> l % 2 == 0;
+        Stream<Long> lst = lstLong.filter(longEqual).limit(10);
 
-        Double sum = lstLong.mapToDouble(d -> d).sum();
+        IntSummaryStatistics stats = lst.mapToInt(v -> v.intValue()).summaryStatistics();
+        System.out.println(stats.getMin());
+        System.out.println(stats.getMax());
+        System.out.println(stats.getAverage());
+        System.out.println(stats.getCount());
+        System.out.println(stats.getSum());
+    }
 
-        //lstLong.forEach(print);
-        //lstLong.forEach(print);
+    public static Double getSumOf12RandomEqualNumbers() {
+        Supplier<Double> ran = () -> Math.random(); //Supplier er et Interface I Java, et såkaldt functional interface. Udfører blot en function.
+        Stream<Double> strran = Stream.generate(ran); //Stream er en ny klasse der blev introduceret I Java samtidig med håndtering af functions/lambda (I Java 8 i 2014, 20år efter første version af Java).
+        Function<Double, Long> dblround = d -> Math.round(d*100); //Function er igen et functional interface. Hvis function I modsætning til Supplier, returnerer en type
+        Predicate<Long> longEqual = l -> l % 2 == 0; //Predicate er et functional interface. Som returnerer en Boolean. Bruges tit I filter til at fjerne alle elementer der returnere false.
 
-        System.out.println(sum);
+        return Stream.generate(ran) //Efter ovenstående forEach er streamen brugt. Så vi er nødt til at lave en ny
+                .map(dblround)
+                .filter(longEqual)
+                .limit(12)
+                .mapToDouble(d -> d) //mapToDouble tager long som input og returner som double. Fordi sum() kun håndterer double. Og vores Math.round returnerede Long.
+                .sum(); //beregner sum og lukker stream.
+    }
 
-
+    public static void main(String[] args) {
+        generate12EqualNumbers();
+        //infiniteLoop();
+        System.out.println(getSumOf12RandomEqualNumbers());
+        useAggregateFunctions();
     }
 }
 
--- src/MainG7.java
+++ src/MainG7.java
@@ -7,9 +7,13 @@ public class MainG7 {
 
     public static void main(String[] args) {
         List<String> G7 = Arrays.asList("Usa", "Japan", "France", "Germany", "Italy", "U.K", "Canada");
-        Stream<String> sss = G7.stream();
+
+        Stream<String> sss = G7.parallelStream();
+
         sss = sss.map(s -> s.toUpperCase(Locale.ROOT));
         sss.forEach(s -> System.out.println(s));
+
+
 
     }
 }
